391c423cd6599bfc26a41054d5deafaa07a3539d
Vo7ice <Vo7ice@outlook.com>
2017-07-07 18:07:34 +0800

    add demo

--- View/src/main/java/com/java/io/practise/MainActivity.java
+++ View/src/main/java/com/java/io/practise/MainActivity.java
@@ -1,15 +1,72 @@
 package com.java.io.practise;
 
-import android.content.Intent;
-import android.os.UserHandle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.java.io.practise.view.CircleIndicatorView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private CircleIndicatorView mCircleIndicatorView, mCircleIndicatorView1,
+            mCircleIndicatorView2, mCircleIndicatorView3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mCircleIndicatorView = (CircleIndicatorView) findViewById(R.id.indicator_view);
+        mCircleIndicatorView1 = (CircleIndicatorView) findViewById(R.id.indicator_view1);
+        mCircleIndicatorView2 = (CircleIndicatorView) findViewById(R.id.indicator_view2);
+        mCircleIndicatorView3 = (CircleIndicatorView) findViewById(R.id.indicator_view3);
+    }
+
+    private class MyPagerAdapter extends PagerAdapter {
+
+        private List<Integer> mResList;
+
+        @Override
+        public int getCount() {
+            return mResList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = LayoutInflater.from(container.getContext()).inflate(R.layout.view_pager_item,null);
+            ImageView image = (ImageView) view.findViewById(R.id.image_item);
+            image.setImageResource(mResList.get(position));
+            container.addView(image);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        public void setData(List<Integer> reaList) {
+            mResList = reaList;
+        }
+
+
     }
 }
